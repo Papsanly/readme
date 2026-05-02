3c794ff709395ad8099ba6fbcb0e4d3e6e5ae85f
Papsanly <papsanlysenko@gmail.com>
2026-05-02 18:16:38 +0300

    add ts ignore message about no types in eslint-config-expo

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,4 +1,5 @@
 const { defineConfig } = require('eslint/config');
+// @ts-expect-error - no types published for eslint-config-expo/flat
 const expoConfig = require('eslint-config-expo/flat');
 const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
 
