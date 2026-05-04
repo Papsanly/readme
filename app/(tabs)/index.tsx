0bbfa5725a6325b52dea3da368071e5a54596d50
Papsanly <papsanlysenko@gmail.com>
2026-05-04 12:14:47 +0300

    add tabs layout

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,38 @@
+import { StyleSheet, View } from 'react-native';
+import { Image } from 'expo-image';
+import PlaceholderImage from '@/assets/images/background-image.png';
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  text: {
+    color: '#fff'
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff'
+  },
+  imageContainer: {
+    flex: 1
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18
+  }
+});
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,18 @@
 import { Stack } from 'expo-router';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#25292e'
+        },
+        headerShadowVisible: false,
+        headerTintColor: '#fff'
+      }}
+    >
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="+not-found" options={{ title: 'Oops! Not Found' }} />
+    </Stack>
+  );
 }
